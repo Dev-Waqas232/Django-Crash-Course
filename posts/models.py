ad3d61879369693ca100b36ccc3cd40c35c4889c
Waqas Munir <devwaqas232@gmail.com>
2024-08-02 11:37:02 +0500

    Created Basic Post Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # Whenever you assign fields, think about web forms and what type of fields you are going to use like input type text or textarea etc
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
